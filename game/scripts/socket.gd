f67cc6ecaff0c9737e394636c0dc4e35fb0af8ec
tim <tim@timschupp.de>
2023-02-04 13:20:39 +0100

    more adotion of the urls

--- game/scripts/socket.gd
+++ game/scripts/socket.gd
@@ -2,7 +2,7 @@ extends Node
 
 #const BACKEND_URL = "http://localhost:8000"
 const BACKEND_URL = "https://t1m.me"
-const SOCKET_URL = "ws://t1m.me/ws/game/"
+const SOCKET_URL = "wss://t1m.me/ws/game/"
 #const SOCKET_URL = "ws://localhost:8000/ws/game/"
 
 var _client = WebSocketClient.new()
